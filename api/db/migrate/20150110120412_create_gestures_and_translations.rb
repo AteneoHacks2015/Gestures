0338fc973c5e032b2c0d0700586778adb81b859a
nmcalabroso <nmcalabroso@outlook.ph>
2015-01-11 01:18:31 +0800

    Updates migration in gestures and translations

--- api/db/migrate/20150110120412_create_gestures_and_translations.rb
+++ api/db/migrate/20150110120412_create_gestures_and_translations.rb
@@ -1,16 +1,17 @@
 class CreateTranslationsAndGestures < ActiveRecord::Migration
   def change
-  	create_table :translations do |t|
+  	
+    create_table :gestures do |t|
+      t.string :name
+    end
+
+    create_table :translations do |t|
   		t.string :name
   	end
 
-  	create_table :gestures do |t|
-  		t.string :name
-  	end
-
-    create_table :translations_gestures do |t|
-    	t.belongs_to :translation, index: true
+    create_table :gestures_translations do |t|
     	t.belongs_to :gesture, index: true
+      t.belongs_to :translation, index: true
     end
 
     add_reference :translations, :item, index: true
